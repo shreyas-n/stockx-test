40ae4df70e7d57137af8f650e6c46225cb5d8594
Shreyas Narlanka <shreyasnarlanka@gmail.com>
2021-06-08 10:19:49 -0700

    j

--- config/initializers/algolia.rb
+++ config/initializers/algolia.rb
@@ -1,2 +1,2 @@
-client = Algolia::Search::Client.create('XW7SBCT9V6', ENV['STOCKX_'])
+client = Algolia::Search::Client.create('XW7SBCT9V6', ENV['STOCKX_API_KEY'])
 STOCKX_ALGOLIA_INDEX = client.init_index('products')
